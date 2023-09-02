be5ec40b3b7e5067440b19576c9c401f9bd94e3f
n00veloper <lucas@javalu.net>
2023-09-02 10:58:14 -0300

    added comments to the code for better view.

--- editor/main.py
+++ editor/main.py
@@ -11,6 +11,7 @@ def main_window():
     cache.canvas = tk.Canvas(cache.root)
     cache.canvas.pack(fill="both", expand=True)
 
+    # menu when clicking on the root window, it has a few commands who prompt on the screen. 
     menu = tix.Menu(cache.root)
     menu.add_command(label="Add frame", command=blocks.create_frame_at)
     menu.add_separator()
@@ -18,11 +19,12 @@ def main_window():
     menu.add_separator()
     menu.add_command(label="Load", command=lambda: save.loadHistory(cache.dic_el, cache.root))
 
+    # manu del is cached because it is used globally, but is set here, used for frame manipulation on right click (mouse-3)
     cache.menu_del = tix.Menu(cache.root)
     cache.menu_del.add_command(label="Remove frame", command=blocks.delete_frame)
 
+    # canvas is a fake "root window" so it does not open two menu on right click.
     cache.canvas.bind("<Button-3>", lambda event: blocks.show_menu(event, menu))
-    #root.bind("<B2-Motion>", lambda event: drag_all(event, dic_el, root))
 
     cache.root.mainloop()
 
